bbb5d1a02a755ff877d929379af661aa89427b65
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-07-11 16:35:17 +0200

    Created the Users seeder

--- db/seeders/20220711143209-users.js
+++ db/seeders/20220711143209-users.js
@@ -2,23 +2,23 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert("Users", [
+      {
+        username: "johndoe1",
+        firstName: "John",
+        lastName: "Doe 1",
+        email: "example1@example.com",
+        password: "$2y$10$mj1OMFvVmGAR4gEEXZGtA.R5wYWBZTis72hSXzpxEs.QoXT3ifKSq", // password
+        status: 1,
+        gender: "f",
+        phoneNumber: "0239239249239",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ]);
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete("Users", null, {});
   }
 };
