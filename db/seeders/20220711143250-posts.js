0558e57727d4c89200cdc62aaa9bafb1e6e4f59d
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-07-11 16:38:57 +0200

    Worked on posts seeder

--- db/seeders/20220711143250-posts.js
+++ db/seeders/20220711143250-posts.js
@@ -2,23 +2,20 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert("Posts", [
+      {
+        title: "Title Post One",
+        slug: "title-post-one",
+        userId: 1,
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        status: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ])
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete("Posts", null, {});
   }
 };
