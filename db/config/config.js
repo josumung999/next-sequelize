e99f155b651acbc1790cd67285726ab763fd3fa4
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-07-11 17:08:53 +0200

    Working on configurations

--- db/config/config.js
+++ db/config/config.js
@@ -8,6 +8,10 @@ module.exports = {
     dialect: "mysql",
   },
   test: {
+    storage: ':memory',
+    dialect: 'sqlite',
+  },
+  staging: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
